7c01f44054e13e574566a245e0418eb54fdc7684
unknown <s.satou.sys22@morijyobi.ac.jp>
2022-09-27 11:04:47 +0900

    pra2.javaに行われる出力が100度繰り返されるよう修正

--- pra2.java
+++ pra2.java
@@ -1,5 +1,6 @@
 public class pra2{
     public static void main(String[] args){
-            System.out.println("Hello World");
+            for(int Kazoe = 0;Kazoe < 100;Kazoe ++)
+	    System.out.println("Hello World");
     }
 }
